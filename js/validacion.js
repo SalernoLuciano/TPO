31792d80dc4dbaf9a60be5bc9e253f345910c8a8
Luciano Salerno <salerno.luciano@outlook.com.ar>
2022-10-10 17:34:53 -0300

    Fetch de videos de trailer

--- js/validacion.js
+++ js/validacion.js
@@ -9,6 +9,7 @@ function showList(movies){
 	const container = document.querySelector('.news_container')
 
 	movies.map(movie => {
+		getVideo(movie.id)
 		const card = `
 		<div class="news_card">
 			<div class="news_card_head">
@@ -39,7 +40,7 @@ async function searchMovieOrShow(type, query) {
 		const APIUrl = `https://api.themoviedb.org/3/${type}?api_key=${APIKEY}&language=es-MX&query=${query}&page=1`
 		const result = await fetch(APIUrl)
 		const resultParsed = await result.json()
-		console.log(resultParsed)	
+		// console.log(resultParsed)	
 		const movies = resultParsed.results
 		showList(movies)
 	}
@@ -50,7 +51,7 @@ async function getNews() {
 		const APIUrl = `https://api.themoviedb.org/3/trending/all/week?api_key=9772a809e83c417c835dc74456b510a4`
 		const result = await fetch(APIUrl)
 		const resultParsed = await result.json()
-		console.log(resultParsed)	
+		// console.log(resultParsed)	
 		const movies = resultParsed.results
 		showList(movies)
 	} catch (error) {
@@ -65,3 +66,23 @@ buscador.addEventListener('search', function(){
 })
 
 getNews()
+
+async function getVideo(id){
+	try{
+		const videos = await fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${APIKEY}`)
+		const videosParsed = await videos.json()
+		const filtrados = videosParsed.results.filter(video => video.type === 'Trailer')
+
+		filtrados.forEach(video => {
+			const key = video.key
+			const sitio = video.site.toLowerCase()
+			const videoUrl  = `https://${sitio}.com/embed/${key}`
+			const iframe = `<iframe width="560" height="315" src=${videoUrl} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+			const body = document.getElementsByTagName('body')[0]
+			body.innerHTML+= iframe
+		})	
+	}
+	catch(error){
+		console.warn(`Error: ${error}: La pelicula no tiene trailer`)
+	}
+}
